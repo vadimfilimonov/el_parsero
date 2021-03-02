427bab77e3f95f267b30fb47f7a9db652937f5c7
Vadim Filimonov <philimonovvadim@gmail.com>
2021-03-02 23:18:34 +0300

    Refactor modules

--- csv_go.py
+++ csv_go.py
@@ -1,95 +1,52 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-from urllib.request import Request, urlopen, urlretrieve
+from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import re
 import os
 import ssl
 
-# Для того чтобы подключить класс формирования ссылки
-import sys
-# переходим в корень
-sys.path.insert(0, '../')
-
 ssl._create_default_https_context = ssl._create_unverified_context
 
 def delrubbish(x):
-	import re
 	x = re.sub("^\s+|\n|\r|\t|\s*$|\t*$;", '', x)
 	x = re.sub('"', "'", x)
 	return x
 
-def toBlue(x):
-	x = '\033[96m' + x + '\033[0m'
-	return x
-
-def rowsnumber(filename):
-	file = open(filename)
-	content = file.read()
-	content_list = content.split('\n')
-	return len(content_list)
-
 def buildCell(x):
-	x = f'"{str(x)}"'
-	return x
+	return f'"{delrubbish(str(x))}"'
 
 def buildRow(*cells):
-	# Создаем пустую строку
 	row = ''
-	# Проходимся по всем ячейкам
 	for cell in cells:
-		# Каждой ячейке справа добавляем точку с запятой
-		row += buildCell(cell) + ';'
-	# У последнего столбца добавляем перенос строки
+		row += f'{buildCell(cell)};'
 	row += '\n'
 	return row
 
-# Переменные
-site = 'http://clipsite.ru'
-# Переменная, куда будут попадать значения csv
-str_csv = ""
+def createBuildFolder():
+	if not os.path.exists('./build'):
+			os.mkdir('./build')
 
-# Файлы
-# Файл, в котором хранятся ссылки на страницы с категориями
-list_file = open('./build/list.txt', 'r')
-# Список товаров
-if not os.path.exists('./build'):
-	os.mkdir('./build')
-open('./build/result.csv', 'w').close()
-result = open("./build/result.csv", "a")
-# Количество строк в файле со ссылками
-filelength = rowsnumber('./build/list.txt')
-# Обнуляем счетчик
-counter = 0
-# Проходимся по всем ссылкам
-for line in list_file.readlines():
-	# Библиотека beautiful soup конвертит ссылки в нужный нам формат
-	html_doc = urlopen(line).read()
-	soup = BeautifulSoup(html_doc, "html.parser")
+def start():
+	str_csv_body = ""
 
-	# Заголовок страницы
-	title = soup.find('h1').text
-	title = delrubbish(title)
+	list_file = open('./build/list.txt', 'r')
+	createBuildFolder()
+	open('./build/result.csv', 'w').close()
+	result = open("./build/result.csv", "a")
+	for line in list_file.readlines():
+		html_doc = urlopen(line).read()
+		soup = BeautifulSoup(html_doc, "html.parser")
+		title = soup.find('h1').text
+		text = soup.find('div', 'field--name-body')
+		image = soup.find('div', 'c-bg-block').get('style')
+		str_csv_body += buildRow(title, text, image)
+		print(title)
+	str_csv_header = buildRow('Заголовок', 'Текст', 'Изображение')
+	result.write(str_csv_header)
+	result.write(str_csv_body)
+	list_file.close()
+	result.close()
 
-	# Текст
-	text = soup.find('div', 'node__content').find('div', 'field--name-body')
-
-	# Изображение
-	image = soup.find('div', 'c-bg-block').get('style')
-	image = delrubbish(image)
-
-	# Поднимаем счетчик на 1
-	counter += 1
-	# Выводим в консоль
-	print(toBlue(str(counter) + '/' + str(filelength) + ': ') + title)
-	# Добавляем столбцы в csv
-	str_csv += buildRow(str(counter), title, text, image)
-
-# Первая строка таблицы - заголовки столбцов
-str_csv_header = buildRow('id', 'Заголовок', 'Текст', 'Изображение')
-result.write(str_csv_header)
-# Записываем конечный csv файл с категориями
-result.write(str_csv)
-list_file.close()
-result.close()
+start()
--- list_go.py
+++ list_go.py
@@ -1,27 +1,36 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-
-# Для того чтобы подключить класс формирования ссылки
 import sys
 import os
-# переходим в корень
-sys.path.insert(0, '../')
-# Подключаем класс
-from links import LinksFormatter
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
 
-if not os.path.exists('./build'):
-	os.mkdir('./build')
+def createBuildFolder():
+	if not os.path.exists('./build'):
+			os.mkdir('./build')
 
-# Переменные
+def goCreateLinks(site, file_input, file_output, linkkeeper):
+	input = open(file_input, "r")
+	for link in input.readlines():
+		html_doc = urlopen(link).read()
+		soup = BeautifulSoup(html_doc, "html.parser")
+		lists = soup.find_all(class_=linkkeeper)
+		links = ''
+		for list_item in lists:
+			link = 'http://' + site + list_item.find('a').get('href')
+			links += link + '\n'
+			print(link)
+		open(file_output, 'w').close()
+		categories_list_result = open(file_output, 'a+')
+		categories_list_result.write(links)
+		categories_list_result.close()
 
-# Сайт, с которого происходит парсинг
-site = 'clipsite.ru'
-# Файл, в котором содержится перечень ссылок, из которых формируется конечный файл с ссылками
-file_input = 'list_start.txt'
-# Файл, в который помещается результат
-file_output = './build/list.txt'
-# Класс ссылок, которые нужно помещать файл
-linkKeeperClass = "c-blog__button"
-# Вызов функции, которая формирует файл
-LinksFormatter.goCreateLinks(site, file_input, file_output, linkKeeperClass)
+def start():
+	createBuildFolder()
+	site = 'clipsite.ru'
+	file_input = 'list_start.txt'
+	file_output = './build/list.txt'
+	linkKeeperClass = "c-blog__button"
+	goCreateLinks(site, file_input, file_output, linkKeeperClass)
 
+start()
